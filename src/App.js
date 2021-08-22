2976782ebfa5675d4115d3fdad1bcda21f971563
arimyl <ari.myl.123@outlook.jp>
2021-08-22 01:52:18 +0000

    20210822/react_station9/commit01

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@
 
 import * as React from 'react'
 import './App.css'
-
+import './DogImage'
+import './Description'
+import './DogListContainer'
+import { useState } from 'react'
+import { DogListContainer } from './DogListContainer'
+// import axios from "axios";
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
+
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/mountain-bernese/n02107683_5960.jpg");
+
+  const getDogUrl = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setDogUrl(data.message)
+      });
+  }
+
   return (
-    <div>
-      <h2>Hello, world!</h2>
+
+    <div className="container">
+      <header className="header">React-stations</header>
+      <div className="dog-div">
+        <button className="update-btn" onClick={() => getDogUrl()}>更新</button>
+        <p>Dogs image.</p>
+        <div className="img-container">
+          <img className="dog-img" src={dogUrl} alt="dog-api random-image" />
+        </div>
+      </div>
+      <DogListContainer />
     </div>
   )
 }
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,47 @@
 // DO NOT DELETE
+import * as React from 'react'
+import './App.css'
+import './DogImage'
+import './Description'
+import { useState } from 'react'
+import { useEffect } from 'react'
+// import axios from "axios";
+/**
+ * 
+ * @type {React.FC}
+ */
+export const DogListContainer = () => {
+  const[breeds, setBreeds] = useState([]);
+
+  // fetch('https://dog.ceo/api/breeds/list/all')
+  // .then(res => res.json())
+  // .then(data => {
+  //   setBreeds([data.message])
+  //   console.log(data)
+  // })
+  
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(res => res.json())
+    .then(data => {
+      setBreeds(()=> (
+        Object.keys(data.message).map(key => key)
+      ))
+      console.log(data)
+    })
+  }, []) 
+  console.log(breeds)
+
+  return(
+    <div>
+      <ul>
+        {/* {
+          Object.keys(breeds).map((key) => (
+            // <li key={key}>{breeds[key]}</li>
+            <li key={key}>{key}</li>
+          ))
+        } */}
+      </ul>
+    </div>
+  )
+}
